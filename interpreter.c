04608f0ad7ae5237d358534cc48bb35cd89e4eab
esamyllyla <esamyllyla@outlook.com>
2026-07-30 20:51:13 +0300

    interpreter function rewrite

--- interpreter.c
+++ interpreter.c
@@ -12,10 +12,12 @@ struct token
 };
 
 void clear_input_buffer(char *input, int size);
-int interpreter(char *input, int input_length);
+int interpreter(char *input, int input_length, int *result);
 struct token get_token(char *input, int token_index);
 void eat_token(char *input, int *token_index, struct token *token);
 int update_result(int operation, int left_operand, int right_operand);
+int fetch_term(char *input, int *token_index, int *result, int input_length);
+int fetch_operator(char *input, int *token_index, int *operator, int input_length);
 
 int main()
 {
@@ -29,13 +31,19 @@ int main()
         printf("calc>");
         fgets(input, sizeof(input), stdin);
     
+        int result;
+    
         while(input[input_length] != '\n')
         {
             input_length++;
         }
-        if(interpreter(input, input_length) == 0)
+        if(interpreter(input, input_length, &result) == 0)
         {
-            printf("Invalid input!\n");
+            printf("Syntax error!\n");
+        }
+        else
+        {
+            printf("%d\n", result);
         }
     }
 
@@ -48,6 +56,52 @@ void clear_input_buffer(char *input, int size)
     {
         input[i] = ' ';
     }
+}
+
+/*
+Summing up arbitrary amount of plus and minus operations
+1                | left: 1
+1 + 3            | left: 1, right: 3, op: +, res: 4
+1 + 3 - 2        | left: 4, right: 2, op: -, res: 2
+1 + 3 - 2 + 7    | left: 2, right: 7, op: +, res: 9
+*/
+
+int interpreter(char *input, int input_length, int *result)
+{
+    int token_index = 0;
+    int left_operand = 0;
+    int right_operand = 0;
+
+    struct token current_token;
+
+    int operator_eaten = 0;
+    int operation = 0;
+
+    int term;
+    int term2;
+
+    if(fetch_term(input, &token_index, &term, input_length))
+    {
+        while(token_index < input_length)
+        {
+            if(fetch_operator(input, &token_index, &operation, input_length))
+            {
+                if(!fetch_term(input, &token_index, &term2, input_length))
+                {
+                    return 0;
+                }
+                else
+                {
+                    term = update_result(operation, term, term2);
+                    *result = term;
+                }
+            }
+            else return 0;
+        }
+        *result = term;
+        return 1;
+    }
+    else return 0;
 }
 
 int update_result(int operation, int left_operand, int right_operand)
@@ -65,68 +119,62 @@ int update_result(int operation, int left_operand, int right_operand)
     return left_operand;
 }
 
-/*
-Summing up arbitrary amount of plus and minus operations
-1                | left: 1
-1 + 3            | left: 1, right: 3, op: +, res: 4
-1 + 3 - 2        | left: 4, right: 2, op: -, res: 2
-1 + 3 - 2 + 7    | left: 2, right: 7, op: +, res: 9
-*/
-
-int interpreter(char *input, int input_length)
+int fetch_term(char *input, int *token_index, int *result, int input_length)
+// Returns boolean telling if succeeded
 {
-    int token_index = 0;
-    int left_operand = 0;
-    int right_operand = 0;
-
-    struct token current_token;
-
-    int operator_eaten = 0;
-    int operation = 0;
-
-    while(token_index < input_length)
+    while(input[*token_index] == ' ' && *token_index < input_length)
     {
-        eat_token(input, &token_index, &current_token);
-
-        if(current_token.type == INTEGER)
-        {
-            if(operator_eaten)
-            {
-                right_operand *= 10;
-                right_operand += current_token.value;
-            }
-            else
-            {
-                left_operand *= 10;
-                left_operand += current_token.value;
-            }
-        }
-        if((current_token.type != INTEGER)
-         & (current_token.type != INVALID)
-         & (current_token.type != SPACE))
-        // If token type is NOT integer, space nor invalid, it must be a operator
-        {
-            operator_eaten++;
-            // To sum up arbitrary amount of plus and minus operations
-            // left operand has to be updated if more than one operators
-            // have been eaten
-            if(operator_eaten > 1)
-            {
-                left_operand = update_result(operation, left_operand, right_operand);
-                right_operand = 0;
-                operator_eaten = 1;
-            }
-            operation = current_token.type;
-        }
-        if(current_token.type == INVALID)
-        {
-            return 0;
-        }
+        *token_index = *token_index + 1;
     }
-    left_operand = update_result(operation, left_operand, right_operand);
     
-    printf("%d\n", left_operand);
+    if(!(48 <= input[*token_index] && input[*token_index] <= 57))
+    {
+        return 0;
+    }
+    
+    int term = 0;
+    while(48 <= input[*token_index] && input[*token_index] <= 57)
+    {
+        term *= 10;
+        term += input[*token_index] - 48;
+        
+        *token_index = *token_index + 1;
+    }
+    
+    *result = term;
+    
+    while(input[*token_index] == ' ' && *token_index < input_length)
+    {
+        *token_index = *token_index + 1;
+    }
     return 1;
+}
+
+int fetch_operator(char *input, int *token_index, int *operator, int input_length)
+{
+    while(*token_index < input_length)
+    {
+        if(input[*token_index] == ' ')
+        {
+            *token_index = *token_index + 1;
+            continue;
+        }
+        if(input[*token_index] == '+')
+        {   
+            *operator = PLUS;
+            *token_index = *token_index + 1;
+            return 1;
+        }
+        if(input[*token_index] == '-')
+        {
+            *operator = MINUS;
+            *token_index = *token_index + 1;
+            return 1;
+        }
+        *token_index = *token_index + 1;
+    }
+    
+    return 0;
 }
 
 void eat_token(char *input, int *token_index, struct token *token)
